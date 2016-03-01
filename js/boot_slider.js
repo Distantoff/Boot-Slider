66d8aebd65372a9727e8a75b35e95bb1b564b620
Distantoff <beatmaking@bk.ru>
2016-03-01 13:34:30 +1000

    Распределение файлов

---
+++ js/boot_slider.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+			/* Интервал смены слайдов | The interval slide change */
+	$timeInterval = 3000;
+
+	/* Инициализация | Initialization */
+	$fullItems = 0;
+
+	if ($("#slider_full_items > div").length) {
+		$fullItems = $("#slider_full_items > div");
+	}
+	else {
+		$("#slider_full_items").append( $(".slider_full_item") );
+		$fullItems = $(".slider_full_item");
+	}
+
+	$fullItems.first().addClass("slider_full_items_active");
+	$("#slider_items > div:first").addClass("slider_items_active");
+	
+	$fullItems.each(function(index){
+		$(this).css({"right" : -$(this).outerWidth() * index});
+	});
+
+	$("#slider_items > div").click(function(){
+		var $index = $(this).index();
+		_slideChange($index);
+	});
+
+	/* Назначение кнопок | The buttons */
+	$("#slider_next").click(function(){
+		_slideTo("next");
+	});
+
+	$("#slider_prev").click(function(){
+		_slideTo("prev");
+	});
+
+	/* Создаем интервал и запускаем первый слайд | Create an interval and start the first slide */
+	//$sliderInterval = setInterval(function() {_slideTo("next");}, $timeInterval);
+	//_slideTo(0);
+});
+
+function _slideChange($index) {
+	//clearInterval($sliderInterval);
+	//$sliderInterval = setInterval(function() {_slideTo("next");}, $timeInterval);
+
+	$("#slider_items > div").removeClass("slider_items_active");
+
+	$fullItems.each(function(index){
+		//console.log($index);
+		$(this).removeClass("slider_full_items_active");
+		if (($index - index) == 0) {
+			$(this).addClass("slider_full_items_active");
+
+			$("#slider_items > div").eq($index).addClass("slider_items_active");
+		}
+			
+		$(this).animate({"right" : $(this).outerWidth() * ($index - index)});
+	});
+
+	_statusLoad();
+}
+
+function _slideTo($way) {
+	var $index = $("#slider_full_items > div.slider_full_items_active").index();
+	//console.log($index);
+	var $lastFullItem = $("#slider_full_items > div:last").index();
+	//console.log($lastFullItem);
+	if ($way == "next") {
+		$index += 1;
+		if ($index > $lastFullItem)
+			$index = 0;
+	}
+	else if ($way == "prev") {
+		$index -= 1;
+		if ($index < $("#slider_full_items > div:first").index())
+			$index = $lastFullItem;
+	}
+	_slideChange($index);
+}
+
+function _statusLoad() {
+	var $sliderLoading = $("#slider_loading");
+
+	if ($sliderLoading.length) {
+		$sliderLoading.stop().css({"width" : "0px"});
+		$sliderLoading.animate({"width" : "100%"}, $timeInterval, "linear", function(){ $(this).css({"width" : "0px"}); });
+	}
+}
