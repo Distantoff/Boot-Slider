3fa25feefa90af12c3e9bd904862c3e84035dc66
Distantoff <beatmaking@bk.ru>
2016-03-13 21:46:41 +1000

    Переработан progressbar

--- js/boot_slider.js
+++ js/boot_slider.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
 		$slideFullItems = $(".slider_full_item");
 	}
 
-	$("#slider_items > div:first").addClass("slider_item_active");
-	
+	_slideChange(0);
+
 	$("#slider_items > div").click(function(){
 		var $index = $(this).index();
 		_slideChange($index);
@@ -30,63 +30,87 @@ $(document).ready(function(){
 		_slideTo("prev");
 	});
 
-	/* Создаем интервал и запускаем первый слайд | Create an interval and start the first slide */
-	//$sliderInterval = setInterval(function() { _slideInterval(); }, $timeInterval);
-	//$slideProgress.start();
-	_slideChange(0);
-
 	$("#slider_full").hover(function(){
-	//	$slideProgress.stop();
+		$slideProgress.pause();
 		
 	}, function(){
-	//	$slideProgress.start();
+		$slideProgress.resume();
 	});
 });
 
 var $slideProgress = {
+	progressBar : $("#slider_loading"),
+	isPause : false,
 
-	start : function () {
-		var self = this;
-		this.interval = setInterval(function() { self.intervalFunction(); }, $timeInterval);
+	start : function ($timeInterval) {
+		if (this.isPause == false) {
+			var self = this;
+			this.startAnimation();
+			this.interval = setInterval(function() { self.intervalFunction(); }, $timeInterval);
+		}
+		else {
+			this.stop();
+		}
 	},
 
 	stop : function() {
 		clearInterval(this.interval);
 		delete this.interval;
-		console.log($("#slider_loading").width());
+		this.stopAnimation();
+	},
+
+	pause : function() {
+		clearInterval(this.interval);
+		delete this.interval;
+		this.pauseAnimation();
+		this.isPause = true;
+	},
+
+	resume : function() {
+		if (this.isPause == true) {
+			var residue = $timeInterval - (this.progressBar.width() / $("#slider_full_items").outerWidth() * $timeInterval);
+			var self = this;
+			
+			this.resumeAnimation(residue);
+			this.interval = setInterval(function() { self.intervalFunction(); }, residue);
+			this.isPause = false;
+		}
 	},
 
 	intervalFunction : function() {
 		_slideTo("next");
-		this.show();
-		console.log($("#slider_loading").width());
 	},
 
-	show : function($pause) {
+	startAnimation : function() {
+		this.progressBar = $("#slider_loading");
 
-		var $sliderLoading = $("#slider_loading");
+		if (this.progressBar.length) {
+			this.progressBar.stop().css({"width" : "0px"});
+			this.progressBar.animate({"width" : "100%"}, $timeInterval, "linear", function(){ $(this).css({"width" : "0px"}); });
+		}
+	},
+	stopAnimation : function() {
+		if (this.progressBar.length) {
+			this.progressBar.stop().css({"width" : "0px"});;
+		}
+	},
+	pauseAnimation : function() {
+		if (this.progressBar.length) {
+			this.progressBar.stop();
+		}
+	},
 
-		if ($sliderLoading.length) {
-			if ($pause == true) {
-				console.log("true");
-				$sliderLoading.stop();
-			}
-			else if ($pause == false) {
-				console.log("false");
-				$sliderLoading.animate({"width" : "100%"}, $timeInterval, "linear", function(){ $(this).css({"width" : "0px"}); });			
-			}
-			else {
-				console.log("nothin");
-				$sliderLoading.stop().css({"width" : "0px"});
-				$sliderLoading.animate({"width" : "100%"}, $timeInterval, "linear", function(){ $(this).css({"width" : "0px"}); });
-			}
+	resumeAnimation : function($residue) {
+		if (this.progressBar.length) {
+			this.progressBar.animate({"width" : "100%"}, $residue, "linear", function(){ $(this).css({"width" : "0px"}); });
 		}
 	}
+
 }
 
 function _slideChange($index) {
-	//$slideProgress.stop();
-	//$slideProgress.start();
+	$slideProgress.stop();
+	$slideProgress.start($timeInterval);
 	
 	$("#slider_full_items > div").removeClass("slider_full_item_active_last");
 	var $slideActive = $("#slider_full_items > div.slider_full_item_active");
@@ -105,8 +129,6 @@ function _slideChange($index) {
 	$slideFullItems.eq($index).addClass("slider_full_item_active");
 
 	_slideAnimate($way, "opacity");
-	//console.log($index);
-	//console.log($way);
 }
 
 function _slideTo($way) {
